514454e6a55355e41dcba8332f68f94c9dc7b9bc
Tony Ferraro <adferra24@gmail.com>
2025-05-16 00:20:32 -0400

    fix: lldb config, memory management, add cli cmds

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
+const char *COMMANDS = "COMMANDS\n - insert head <data>\n - insert tail <data>\n - insert <position> <data>\n - delete <position>\n - delete head\n - delete tail\n - print\n - print head\n - print tail\n - length\n - exit\n type 'help' to see this list again\n";
 
 struct Node
 {
@@ -18,10 +20,10 @@ struct List
 struct Node *createNode(char *data)
 {
    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
-   node->data = data;
+   node->data = malloc(strlen(data) + 1);
+   strcpy(node->data, data);
    node->next = NULL;
    node->prev = NULL;
-
    return node;
 }
 
@@ -80,7 +82,7 @@ struct Node *findNodeByPosition(struct List *list, int position)
    return node;
 }
 
-int updateLengthOfList(struct List *list)
+int updateListLength(struct List *list)
 {
    int count = 0;
    struct Node *curr = list->head;
@@ -93,7 +95,7 @@ int updateLengthOfList(struct List *list)
    return list->length;
 }
 
-int updateLengthOfListRecurse(struct Node *head, int currCount, int length)
+int updateListLengthRecurse(struct Node *head, int currCount, int length)
 {
    int count = currCount;
    struct Node *curr = head;
@@ -105,7 +107,7 @@ int updateLengthOfListRecurse(struct Node *head, int currCount, int length)
    }
 
    count++;
-   return updateLengthOfListRecurse(curr->next, count, length);
+   return updateListLengthRecurse(curr->next, count, length);
 }
 
 void insertNodeAtTail(struct List *list, char *data)
@@ -136,7 +138,9 @@ void insertNodeAtTail(struct List *list, char *data)
 
 void insertNodeAtHead(struct List *list, char *data)
 {
-   printf("insertNodeAtHead\n");
+   if (list->head != NULL)
+      printf("insertNodeAtHead: %s\n", list->head->data);
+
    struct Node *newNode = createNode(data);
    if (list->head == NULL)
    {
@@ -148,6 +152,7 @@ void insertNodeAtHead(struct List *list, char *data)
    else
    {
       struct Node *tmp = list->head;
+      printf("node tmp: %s\nnode head: %s\n", tmp->data, list->head->data);
       list->head = newNode;
       tmp->prev = list->head;
       list->head->next = tmp;
@@ -194,15 +199,20 @@ void deleteNodeAtHead(struct List *list)
    {
       if (list->head->next == NULL)
       {
+         free(list->head->data);
+         free(list->head);
          list->head = NULL;
          list->length--;
          return;
       }
 
-      struct Node *tmp = list->head->next;
-      list->head = tmp;
+      struct Node *tmp = list->head;
+      list->head = tmp->next;
       list->head->prev = NULL;
-      list->head->next = tmp->next;
+      if (tmp->next->next != NULL)
+         list->head->next = tmp->next->next;
+      free(tmp->data);
+      free(tmp);
       tmp = NULL;
       list->length--;
    }
@@ -217,17 +227,25 @@ void deleteNodeAtTail(struct List *list)
    {
       if (list->tail == list->head)
       {
+
+         free(list->head->data);
+         free(list->tail->data);
+         free(list->head);
+         free(list->tail);
          list->tail = NULL;
          list->head = NULL;
          list->length--;
          return;
       }
 
-      struct Node *tmp = list->tail->prev;
-      list->tail = tmp;
+      struct Node *tmp = list->tail;
+      list->tail = tmp->prev;
       list->tail->next = NULL;
-      list->tail->prev = tmp->prev;
+      if (tmp->prev != NULL)
+         list->tail->prev = tmp->prev->prev;
 
+      free(tmp->data);
+      free(tmp);
       tmp = NULL;
       list->length--;
    }
@@ -254,7 +272,9 @@ void deleteNodeAtPosition(struct List *list, int position)
                tmp->prev->next = node->next;
             if (tmp->next != NULL && tmp->next->prev != NULL)
                tmp->next->prev = node->prev;
-            node = NULL;
+
+            free(tmp->data);
+            free(tmp);
             tmp = NULL;
             list->length--;
          }
@@ -266,10 +286,115 @@ void deleteNodeAtPosition(struct List *list, int position)
       printf("Error: Delete position out of bounds\n");
 }
 
-int main()
+int main(int argc, const char **argv)
 {
+   char input[1024];
    struct List *list = (struct List *)malloc(sizeof(struct List));
    list->length = 0;
 
+   printf("Create a doubly linked list\n\n");
+   printf("%s", COMMANDS);
+
+   while (strcmp(input, "exit") != 0)
+   {
+      printf("> ");
+      fgets(input, BUFSIZ, stdin);
+      input[strcspn(input, "\n")] = '\0';
+      char *cmd = strtok(input, " ");
+
+      if (strcmp(cmd, "insert") == 0)
+      {
+         char *arg = strtok(NULL, " ");
+         char *end;
+         long int pos = strtol(arg, &end, 10);
+
+         if (strcmp(arg, "head") == 0)
+         {
+            char *data = strtok(NULL, " ");
+            printf("inserting data: %s at head\n", data);
+            insertNodeAtHead(list, data);
+         }
+         else if (strcmp(arg, "tail") == 0)
+         {
+            char *data = strtok(NULL, " ");
+            printf("inserting data: %s at tail\n", data);
+            insertNodeAtTail(list, data);
+         }
+         else if (arg != end)
+         {
+            char *data = strtok(NULL, " ");
+
+            printf("inserting data: %s at position %ld\n", data, pos);
+            insertNodeAtPosition(list, data, pos);
+         }
+         else
+         {
+            printf("invalid insert command\n");
+         }
+      }
+
+      else if (strcmp(cmd, "delete") == 0)
+      {
+         char *arg = strtok(NULL, " ");
+         char *end;
+         long int pos = strtol(arg, &end, 10);
+
+         if (strcmp(arg, "head") == 0)
+         {
+            printf("delete head node\n");
+            deleteNodeAtHead(list);
+         }
+         else if (strcmp(arg, "tail") == 0)
+         {
+            printf("delete tail node\n");
+            deleteNodeAtTail(list);
+         }
+         else if (arg != end)
+         {
+            printf("delete node at position %ld\n", pos);
+            deleteNodeAtPosition(list, pos);
+         }
+         else
+         {
+            printf("invalid delete command\n");
+         }
+      }
+      else if (strcmp(cmd, "print") == 0)
+      {
+         char *arg = strtok(NULL, " ");
+
+         if (arg == NULL)
+         {
+            traverseForward(list);
+         }
+         else if (strcmp(arg, "head") == 0)
+         {
+            if (list->head != NULL)
+               printf("Head node: %s\n", list->head->data);
+            else
+               printf("Head node: NULL");
+         }
+         else if (strcmp(arg, "tail") == 0)
+         {
+            if (list->tail != NULL)
+               printf("Tail node: %s\n", list->tail->data);
+            else
+               printf("Tail node: NULL");
+         }
+      }
+      else if (strcmp(cmd, "length") == 0)
+      {
+         printf("List Length: %d\n", list->length);
+      }
+      else if (strcmp(cmd, "help") == 0)
+      {
+         printf("%s", COMMANDS);
+      }
+      else
+      {
+         printf("command not found\n");
+      }
+   }
+   printf("Exiting...");
    return 0;
 }
